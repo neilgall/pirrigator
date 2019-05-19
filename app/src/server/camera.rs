86c8209ed962ee7bc4577873f5988b3e2a591b26
Neil Gall <neilgall@mac.com>
2019-05-19 13:35:42 +0100

    Fix image caching to expire after 15 mins

--- app/src/server/camera.rs
+++ app/src/server/camera.rs
@@ -3,29 +3,55 @@ use iron::prelude::*;
 use iron::status;
 use iron::mime::*;
 use router::Router;
+use std::io::{Error, ErrorKind, Result};
 use std::process::Command;
+use std::time::{Duration, SystemTime};
 use super::error::internal_error;
 use super::get_param;
 
 const DEFAULT_WIDTH: u16 = 1280;
 const DEFAULT_HEIGHT: u16 = 720;
+const MAX_CACHE_SECONDS: u64 = 900;
 
-cached!{
-	IMAGES: SizedCache<(u16, u16), Vec<u8>> = SizedCache::with_size(5);
-	fn get_image(width: u16, height: u16) -> Vec<u8> = {
-		let output = Command::new("/opt/vc/bin/raspistill")
-			.args(&["-o", "-", "-w", &format!("{}", width), "-h", &format!("{}", height)])
-			.output();
+fn get_live_image(width: u16, height: u16) -> Result<Vec<u8>> {
+	let output = Command::new("/opt/vc/bin/raspistill")
+			.args(&["-o", "-", "-rot", "270", "-w", &format!("{}", width), "-h", &format!("{}", height)])
+			.output()?;
+	Ok(output.stdout)	
+}
 
-		match output {
-			Ok(output) => {
-				output.stdout
+cached_control!{
+	IMAGES: SizedCache<(u16, u16), (SystemTime, Vec<u8>)> = SizedCache::with_size(5);
+
+	Key = { (width, height) };
+
+	PostGet(cached) = {
+		return SystemTime::now().duration_since(cached.0)
+			.map_err(|_| Error::new(ErrorKind::Other, "time error"))
+			.and_then(|duration| {
+				if duration > Duration::from_secs(MAX_CACHE_SECONDS) {
+					Err(Error::new(ErrorKind::NotFound, "expired"))
+				} else {
+					Ok(cached.1.clone())
+				}
 			}
-			Err(e) => {
-				error!("Failed to capture image: {}", e);
-				vec![]
-			}
+		)
+	};
+
+	PostExec(result) = { result };
+
+	Set(result) = { 
+		let timestamp = SystemTime::now();
+		match(result) {
+			Ok(ref image) => (timestamp, image.clone()),
+			Err(_) => (SystemTime::now(), vec![])
 		}
+	};
+
+	Return(result) = { result };
+
+	fn get_image(width: u16, height: u16) -> Result<Vec<u8>> = {
+		get_live_image(width, height)
 	}
 }
 
@@ -36,15 +62,17 @@ fn capture(width: u16, height: u16) -> IronResult<Response> {
 		vec![]
 	);
 
-	let image = get_image(width, height);
-	if image.len() == 0 {
-		Err(internal_error("Failed to capture image"))
-	} else {
-		let mut response = Response::new();
-		response.set_mut(image)
+	match get_image(width, height) {
+		Ok(image) => {
+			let mut response = Response::new();
+			response.set_mut(image)
 				.set_mut(content_type)
 				.set_mut(status::Ok);
-		Ok(response)
+			Ok(response)
+		}
+		Err(e) => {
+			Err(internal_error(&format!("Failed to capture image: {}", e)))
+		}
 	}
 }
 
